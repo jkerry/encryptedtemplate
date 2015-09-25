bdbf69d38421b967d6120e20aa9fc1144c7154fe
John Kerry <jk185160@ncr.com>
2015-09-24 22:08:54 -0400

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'EncryptedTemplate'
+maintainer       'John Kerry'
+maintainer_email 'YOUR_EMAIL'
+license          'Apache 2.0'
+description      'Installs/Configures EncryptedTemplate'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
